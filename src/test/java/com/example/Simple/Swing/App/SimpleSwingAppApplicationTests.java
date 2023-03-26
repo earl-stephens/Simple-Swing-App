bef2547874080a7a2c7031b44c19e8ef1b6f41d7
earl-stephens <stephensearl@hotmail.com>
2023-03-26 14:43:42 -0600

    Initial commit

---
+++ src/test/java/com/example/Simple/Swing/App/SimpleSwingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Simple.Swing.App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSwingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
